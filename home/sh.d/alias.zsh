50bfe7c4ab5664d6b671caaa69abf5808a1f40bb
prxi <prxy@tuta.io>
2018-01-06 19:54:21 +0200

    0x7

--- home/sh.d/alias.zsh
+++ home/sh.d/alias.zsh
@@ -1,11 +1,12 @@
 alias ls='gls -hF --color=auto --group-directories-first'
+alias xquartz='/opt/pkg/bin/startx -- /opt/pkg/bin/Xquartz'
 
 conf(){
 	case $1 in
 	zsh)       $EDITOR ~/.zshrc && source ~/.zshrc ;;
 	alias)     $EDITOR ~/.sh.d/alias.zsh && source ~/.zshrc ;;
 	bspwm)     $EDITOR ~/.config/bspwm/bspwmrc ;;
-	sxhkd)	   $EDITOR ~/.config/sxhkd/sxhkdrc ;;
+	sxhkd)     $EDITOR ~/.config/sxhkd/sxhkdrc ;;
 	xinit)     $EDITOR ~/.xinitrc ;;
 	xdef)      $EDITOR ~/.Xdefaults && xrdb ~/.Xdefaults ;;
 	tmux)      $EDITOR ~/.tmux.conf ;;
